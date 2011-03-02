0662ac612b0b0efb8fd7a4639fc6024004ad7e7d
whitered <dmitry.zhelnin@gmail.com>
2011-03-02 12:33:01 +0300

    initial commit: empty rails 3.0.5 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P27::Application.config.secret_token = '7c5f83411f4a59fd6c5e6b138cdcf570942408025c728193b282497e3ff5f2172feb3bd669acb7f429d2edf526985775d4ccbba91339a1d52a3a95b8b59f7fcc'
