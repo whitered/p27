1f2c7a5cd29b2fa1ad391cc7d6b30da76bab26c5
whitered <dmitry.zhelnin@gmail.com>
2011-03-04 17:08:47 +0300

    layouts/_authentication created, notifications rendering extracted to a template

---
+++ spec/views/layouts/_authentication.html.erb_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+describe 'layouts/_authentication' do
+
+  include Devise::TestHelpers
+
+  let(:page) { Capybara.string rendered }
+  let(:authentication) { page.find '#authentication' }
+
+  context 'for guest' do
+
+    before do
+      render
+    end
+
+    it 'should have login link' do
+      authentication.should have_link(t('authentication.login'), 
+                                      :href => new_user_session_path)
+    end
+
+    it 'should have registration link' do
+      authentication.should have_link(t('authentication.registration'), 
+                                        :href => new_user_registration_path)
+    end
+
+    #it 'should not have profile link' do
+      #authentication.should_not have_xpath("//a[@href = '#{user_profile_path}']")
+    #end
+
+    it 'should not have logout link' do
+      authentication.should_not have_xpath(".//a[@href='#{destroy_user_session_path}']")
+    end
+
+  end
+
+  context 'for user' do
+
+    before do
+      @view.should_receive(:user_signed_in?).and_return(true)
+      render
+    end
+
+    #it 'should have profile link' do
+      #authentication.should have_link(t('authentication.profile'),
+                                      #:href => user_profile_path)
+    #end
+
+    it 'should have logout link' do
+      authentication.should have_link(t('authentication.logout'),
+                                      :href => destroy_user_session_path)
+    end
+
+    it 'should not have login link' do
+      authentication.should_not have_xpath(".//a[@href='#{new_user_session_path}']")
+    end
+
+    it 'should not have registration link' do
+      authentication.should_not have_xpath(".//a[@href = '#{new_user_registration_path}']")
+    end
+
+  end
+
+end
+
