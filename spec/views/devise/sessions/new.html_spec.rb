0d412b8093935b46503678e9a2f19209037ca8bc
whitered <dmitry.zhelnin@gmail.com>
2011-03-05 17:34:21 +0300

    formtastic for login page

--- spec/views/devise/sessions/new.html_spec.rb
+++ spec/views/devise/sessions/new.html_spec.rb
@@ -15,24 +15,37 @@ describe 'devise/sessions/new.html.erb' do
 
   let(:page) { Capybara.string rendered }
 
-  before do
-    render
-  end
 
   it 'should have page title' do
+    render
     page.should have_content(t 'devise.sessions.new.title')
   end
 
   it 'should have field for login' do
+    render
     page.should have_field(t 'activerecord.attributes.user.login')
   end
 
   it 'should have field for password' do
+    render
     page.should have_field(t 'activerecord.attributes.user.password')
   end
 
   it 'should have submit button' do
+    render
     page.should have_button(t 'devise.sessions.new.submit')
+  end
+
+  it 'should render error for login' do
+    user.errors.add(:login)
+    render
+    page.should have_content(t_error(User, :login))
+  end
+
+  it 'should render errors for password' do
+    user.errors.add(:password)
+    render
+    page.should have_content(t_error(User, :password))
   end
 
 end
