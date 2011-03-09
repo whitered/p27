c108fa323a872ff9e19d298262237036dbd21e37
whitered <dmitry.zhelnin@gmail.com>
2011-03-09 17:50:51 +0300

    acceptance test for adding users to group

---
+++ spec/acceptance/add_user_to_group_spec.rb
@@ -0,0 +1,27 @@
+require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
+
+feature "Add User To Group" do
+
+  context 'group admin' do
+
+    before do
+      @group = Group.make!
+      @admin = User.make!
+      @users = User.make!(3)
+      @group.users << @admin
+      @group.set_admin_status @admin, true
+      login @admin
+    end
+
+    scenario 'should be able to add several users to the group by their usernames' do
+      visit group_path(@group.id)
+      fill_in t('groups.add_user.field'), :with => @users.map{ |u| u.username }.join(' ')
+      click_link_or_button t('groups.add_user.commit')
+
+      group_members = page.find('#group_users')
+      group_members.should have_link(@user.username, :href => user_path(@user.username))
+
+    end
+  end
+
+end
