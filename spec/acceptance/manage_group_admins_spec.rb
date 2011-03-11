382862b44a56d8c3862a22684b8c9a3b2bcbf0e2
whitered <dmitry.zhelnin@gmail.com>
2011-03-11 12:35:29 +0300

    views/show.html has links to manage admins for group owner

--- spec/acceptance/manage_group_admins_spec.rb
+++ spec/acceptance/manage_group_admins_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 
-feature "Set Group Admin" do
+feature 'Manage Group Admins' do
 
   context 'group admin' do
 
@@ -14,20 +14,21 @@ feature "Set Group Admin" do
       @user, @admin, @member = User.make!(3)
       @group.users << @user << @admin << @member
       @group.owner = @user
+      @group.save!
       @group.set_admin_status @admin, true
       login @user
     end
 
     scenario 'should be able to set group admin' do
       visit group_path(@group)
-      find_member('Member').click_link_or_button t('groups.set_admin.link.name')
-      find_member('Member').should have_selector('.admin')
+      find_member(@member.username).click_link_or_button t('groups.set_admin.set_link.name')
+      find_member(@member.username).should have_selector('.admin')
     end
 
-    scenario 'should be able to fire group admin' do
+    scenario 'should be able to unset group admin' do
       visit group_path(@group)
-      find_member('Admin').click_link_or_button t('groups.unset_admin.link.name')
-      find_member('Admin').should have_no_selector('.admin')
+      find_member(@admin.username).click_link_or_button t('groups.set_admin.unset_link.name')
+      find_member(@admin.username).should have_no_selector('.admin')
     end
  
   end
