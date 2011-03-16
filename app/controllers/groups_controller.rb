75dd6901961b67f9ae0f5c71283e47b407ba2e93
whitered <dmitry.zhelnin@gmail.com>
2011-03-17 01:57:18 +0300

    unused actions remove from groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -66,47 +66,6 @@ class GroupsController < ApplicationController
     redirect_to group
   end
 
-
-  def add_user
-    group = Group.find(params[:id])
-    if !group.user_is_admin?(current_user)
-      flash[:error] = t('groups.add_user.errors.not_permitted')
-    elsif params[:name].nil?
-      flash[:error] = t('groups.add_user.errors.no_name_given')
-    else
-      usernames = params[:name].split(/\W+/)
-      wrong_names = []
-      usernames.each do |username|
-        user = User.find(:first, :conditions => [ 'lower(username) = ?', username.downcase ])
-        if user.nil?
-          wrong_names << username
-        else
-          group.users << user unless group.users.exists?(user)
-        end
-      end
-      flash[:error] = t('groups.add_user.errors.wrong_names', :names => wrong_names.join(', ')) unless wrong_names.empty?
-    end
-    redirect_to group
-  end
-
-  def remove_user
-    group = Group.find(params[:id])
-    if !group.user_is_admin?(current_user)
-      flash[:error] = t('groups.remove_user.errors.not_permitted')
-    elsif params[:username].nil?
-      flash[:error] = t('groups.remove_user.errors.no_name_given')
-    else
-      user = group.users.find(:first, :conditions => [ 'lower(username) = ?', params[:username].downcase ])
-      if user.nil?
-        flash[:error] = t('groups.remove_user.errors.user_not_found', :username => params[:username])
-      else
-        group.users.delete user
-        flash[:notice] = t('groups.remove_user.successful', :username => user.username)
-      end
-    end
-    redirect_to group
-  end
-
   def manage_admins
     group = current_user.groups.find_by_id(params[:id]) || current_user.own_groups.find_by_id(params[:id])
     raise ActiveRecord::RecordNotFound if group.nil?
