1449de3fb76dae091416138ff0864aaac36b88e3
whitered <dmitry.zhelnin@gmail.com>
2011-03-21 17:40:25 +0300

    specs and invitation partial fixed

--- spec/acceptance/user_invitations_spec.rb
+++ spec/acceptance/user_invitations_spec.rb
@@ -4,17 +4,18 @@ feature "User Invitations" do
 
   background do
     @user = User.make!
+    @groups = Group.make!(2)
   end
 
 
   scenario 'user views his invitations' do
-    Invitation.create(:user => @user, :author => User.make!, :group => @group1)
-    Invitation.create(:user => @user, :author => User.make!, :group => @group2)
+    Invitation.create(:user => @user, :author => User.make!, :group => @groups.first)
+    Invitation.create(:user => @user, :author => User.make!, :group => @groups.second)
 
     login @user
     visit invitations_path
-    page.should have_content(@group1.name)
-    page.should have_content(@group2.name)
+    page.should have_content(@groups.first.name)
+    page.should have_content(@groups.second.name)
   end
 
 
--- spec/views/invitations/_invitation.html.haml_spec.rb
+++ spec/views/invitations/_invitation.html.haml_spec.rb
@@ -4,25 +4,30 @@ describe 'invitations/_invitation' do
 
   before do 
     @invitation = Invitation.make!(:group => Group.make!, :user => User.make!, :author => User.make!)
-    render
+    render :partial => 'invitations/invitation.html.haml', :locals => { :invitation => @invitation }
   end
 
   let(:page) { Capybara.string rendered }
+  let(:node) { page.find('.invitation') }
+
+  it 'should have class invitations' do
+    page.should have_selector('.invitation')
+  end
 
   it 'should render group name' do
-    page.should have_content(@invitation.group.name)
+    node.should have_content(@invitation.group.name)
   end
 
   it 'should render author username' do
-    page.should have_content(@invitation.author.username)
+    node.should have_content(@invitation.author.username)
   end
 
   it 'should have link to accept invitation' do
-    page.should have_link(t('invitations.invitation.accept'), :href => accept_invitation_path(@invitation))
+    node.should have_link(t('invitations.invitation.accept'), :href => accept_invitation_path(@invitation))
   end
 
   it 'should have link to decline invitation' do
-    page.should have_link(t('invitations.invitation.decline'), :href => decline_invitation_path(@invitation))
+    node.should have_link(t('invitations.invitation.decline'), :href => decline_invitation_path(@invitation))
   end
 
 end
