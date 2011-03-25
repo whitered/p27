a6fd65b0982eb7bab04c670915156523a290d967
whitered <dmitry.zhelnin@gmail.com>
2011-03-25 16:31:31 +0300

    invitation controller behavior with group members improved

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -11,38 +11,45 @@ class InvitationsController < ApplicationController
     @group = Group.find(params[:group_id])
     raise ActiveRecord::RecordNotFound unless @group.user_is_admin?(current_user)
 
-    sent_invitations = []
-    wrong_invitations = []
-    params[:recipients].split(/[\s;,]+/).each do |name|
-      user = User.find_by_username_or_email(name)
-      if user.nil?
-        invitation = Invitation.new(:email => name)
-      else 
-        invitation = Invitation.new(:user => user)
-      end
-      invitation.group = @group
-      invitation.inviter = current_user
-      if invitation.save
-        InvitationMailer.invite_user(invitation, new_user_registration_url(:code => invitation.code)).deliver
-        sent_invitations << name
-      else
-        wrong_invitations << name
-      end
-    end
+    sent_invitations = 0
+    wrong_emails = []
+    members = []
+    
+    recipients = params[:recipients].blank? ? [] : params[:recipients].split(/[\s;,]+/)
 
-    if wrong_invitations.empty?
-      flash[:notice] = (sent_invitations.size > 1) ? t('invitations.create.invitations_sent') : t('invitations.create.invitation_sent')
+    if recipients.empty?
+      flash[:alert] = t('invitations.create.no_recipients')
     else
-      if wrong_invitations.size > 1
-        i18key = 'invitations.create.wrong_emails'
-        i18params = {:recipients => wrong_invitations.join(', ')}
-      else
-        i18key = 'invitations.create.wrong_email'
-        i18params = {:recipient => wrong_invitations.first}
+      recipients.each do |name|
+        user = User.find_by_username_or_email(name)
+        if user.nil?
+          invitation = Invitation.new(:email => name, :group => @group, :inviter => current_user)
+          if invitation.save
+            InvitationMailer.invite_user(invitation, new_user_registration_url(:code => invitation.code)).deliver
+            sent_invitations += 1
+          else
+            wrong_emails << name
+          end
+        elsif user.groups.exists?(@group)
+          members << name
+        else 
+          invitation = Invitation.create(:user => user, :group => @group, :inviter => current_user)
+          sent_invitations += 1
+        end
       end
-      flash[:error] = t(i18key, i18params)
-    end
 
+      if sent_invitations == recipients.size
+        flash[:notice] = sent_invitations > 1 ? 
+          t('invitations.create.invitations_sent') : 
+          t('invitations.create.invitation_sent')
+      elsif wrong_emails.size + members.size > 1
+        flash[:alert] = t('invitations.create.invitations_failed', :recipients => [wrong_emails + members].join(', '))
+      elsif wrong_emails.any?
+        flash[:alert] = t('invitations.create.wrong_email', :recipient => wrong_emails.first)
+      elsif members.any?
+        flash[:alert] = t('invitations.create.user_is_member', :recipient => members.first)
+      end                  
+    end
 
     render :new
   end
@@ -53,8 +60,7 @@ class InvitationsController < ApplicationController
 
   def accept
     invitation = current_user.invitations.find(params[:id])
-    Membership.create(:user => current_user, :group => invitation.group, :inviter => invitation.inviter)
-    invitation.destroy
+    invitation.accept!
     flash[:notice] = t('invitations.accept.successful', :group => invitation.group.name)
     redirect_to invitations_path
   end
