dd50cfd4b6bf423b598c3040ea498fb10ffee977
whitered <dmitry.zhelnin@gmail.com>
2011-03-28 16:19:13 +0400

    post model implemented

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should have title' do
+    Post.new.should respond_to(:title)
+  end
+
+  it 'should have body' do
+    Post.new.should respond_to(:body)
+  end
+
+  it 'should have author' do
+    Post.new.should respond_to(:author)
+  end
+
+  describe 'author' do
+    it 'should not be nil' do
+      post = Post.new
+      post.should be_invalid
+      post.errors[:author_id].should_not be_empty
+      post.author = User.make!
+      post.should be_valid
+    end
+  end
+
+  it 'should have group' do
+    Post.new.should respond_to(:group)
+  end
+
 end
