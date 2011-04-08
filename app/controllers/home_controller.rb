d55ae3f7cee4d5b6fd1977a3257c4a5774e15ba8
whitered <dmitry.zhelnin@gmail.com>
2011-04-08 15:31:54 +0400

    home_controller#index renders posts from user groups

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,9 @@ class HomeController < ApplicationController
   skip_before_filter :authenticate_user!, :only => :index
 
   def index
+    if user_signed_in?
+      @posts = Post.joins(:group => {:memberships => :user}).where(:memberships => {:user_id => current_user.id}).order('created_at DESC')
+    end
   end
 
 end
