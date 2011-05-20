5d67f1419c57b137d08dbdce64eaa5b64c5b61b4
whitered <dmitry.zhelnin@gmail.com>
2011-05-20 17:47:28 +0400

    sidebar does not use instance variables

--- spec/views/layouts/_sidebar.html.haml_spec.rb
+++ spec/views/layouts/_sidebar.html.haml_spec.rb
@@ -2,22 +2,16 @@ require 'spec_helper'
 
 describe 'layouts/_sidebar' do
 
-  before do
+  subject do
     stub_template 'groups/_group.html.haml' => '%div[group]'
+    render 'layouts/sidebar', :my_groups => Group.make(2)
+    Capybara.string rendered
   end
-
-  let(:page) { Capybara.string rendered }
 
   it 'should render user groups' do
-    @my_groups = Group.make!(2)
-    render
-    page.all('.group').size.should == 2
+    subject.all('.group').size.should == 2
   end
 
-  it 'should have link to my groups' do
-    @my_groups = []
-    render
-    page.should have_link(t('layouts.sidebar.my_groups', :href => my_groups_path))
-  end
+  it { should have_link(t('layouts.sidebar.my_groups', :href => my_groups_path)) }
 
 end
