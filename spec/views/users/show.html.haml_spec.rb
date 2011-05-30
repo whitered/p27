5ee0f95c6724efe9bb7f8fbe587e2cdc61323e5e
whitered <dmitry.zhelnin@gmail.com>
2011-05-30 18:35:39 +0400

    own user page shows link to invitations

--- spec/views/users/show.html.haml_spec.rb
+++ spec/views/users/show.html.haml_spec.rb
@@ -7,7 +7,7 @@ describe "users/show.html.haml" do
   let(:submenu) { Capybara.string view.instance_variable_get(:@_content_for)[:submenu] }
 
   before do
-    @user = User.make
+    @user = User.make!
   end
 
   def content_for name
@@ -19,10 +19,42 @@ describe "users/show.html.haml" do
     content_for(:title).should include(@user.username)
   end
 
-  it 'should have link to edit profile page if user is logged in' do
-    sign_in @user
-    render
-    submenu.should have_link(t('users.show.edit_profile'), :href => edit_user_registration_path)
+  context 'for own profile' do
+
+    before do
+      sign_in @user
+    end
+  
+    it 'should have link to edit profile page' do
+      render
+      submenu.should have_link(t('users.show.edit_profile'), :href => edit_user_registration_path)
+    end
+
+    it 'should have link to invitations index' do
+      render
+      submenu.should have_link(t('users.show.invitations'), :href => invitations_path)
+    end
+
+  end
+
+  context 'for foreign profile' do
+
+    before do
+      sign_in User.make!
+    end
+
+    it 'should not have link to edit profile' do
+      render
+      submenu.should have_no_link(t('users.show.edit_profile'))
+      submenu.should have_no_xpath(".//a[@href='#{edit_user_registration_path}']")
+    end
+
+    it 'should not have link ot invitations path' do
+      render
+      submenu.should have_no_link(t('users.show.invitations'))
+      submenu.should have_no_xpath(".//a[@href='#{invitations_path}']")
+    end
+
   end
 
   it 'should have user name' do
